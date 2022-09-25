a4e4c8aeee818cbe4856af2b9bc16b2d975dee26
yaoxiawen <735649296@qq.com>
2022-09-25 12:25:50 +0800

    项目初步搭建

---
+++ app/src/main/java/com/example/composestatedemo/todo/Data.kt
@@ -0,0 +1,30 @@
+package com.example.composestatedemo.todo
+
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.composestatedemo.R
+import java.util.*
+
+data class TodoItem(
+    val task: String,
+    val icon: TodoIcon = TodoIcon.Default,
+    val id: UUID = UUID.randomUUID()
+)
+
+enum class TodoIcon(
+    val imageVector: ImageVector,
+    @StringRes val contentDescription: Int
+) {
+    //使用Material Design的图标
+    Square(Icons.Default.CropSquare, R.string.cd_expand),
+    Done(Icons.Default.Done, R.string.cd_done),
+    Event(Icons.Default.Event, R.string.cd_event),
+    Privacy(Icons.Default.PrivacyTip, R.string.cd_privacy),
+    Trash(Icons.Default.RestoreFromTrash, R.string.cd_restore);
+
+    companion object {
+        val Default = Square
+    }
+}
