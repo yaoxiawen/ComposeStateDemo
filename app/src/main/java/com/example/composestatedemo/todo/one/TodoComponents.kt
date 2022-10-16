a79047a4c6390f5665c18a57dd8cb12800250b9a
yaoxiawen <735649296@qq.com>
2022-10-16 15:21:19 +0800

    输入框下方图标

--- app/src/main/java/com/example/composestatedemo/todo/one/TodoComponents.kt
+++ app/src/main/java/com/example/composestatedemo/todo/one/TodoComponents.kt
@@ -1,8 +1,13 @@
 package com.example.composestatedemo.todo.one
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -11,7 +16,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.composestatedemo.todo.TodoIcon
 import com.example.composestatedemo.todo.TodoItem
 
 @Composable
@@ -24,6 +32,10 @@ fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
     val (text, setText) = remember {
         mutableStateOf("")
     }
+    val (icon, setIcon) = remember {
+        mutableStateOf(TodoIcon.Default)
+    }
+    val iconsVisible = text.isNotBlank()
     Column {
         Row(
             modifier = Modifier
@@ -46,6 +58,15 @@ fun TodoItemInput(onItemComplete: (TodoItem) -> Unit) {
                 modifier = Modifier.align(Alignment.CenterVertically),
                 enabled = text.isNotBlank()
             )
+        }
+        if (iconsVisible) {
+            AnimatedIconRow(
+                icon = icon,
+                onIconChange = setIcon,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        } else {
+            Spacer(modifier = Modifier.height(16.dp))
         }
     }
 }
@@ -86,5 +107,90 @@ fun TodoEditButton(
         enabled = enabled
     ) {
         Text(text = text)
+    }
+}
+
+/**
+ * 一行图标，根据文本是否有内容，自动收起和弹出，带动画效果
+ */
+@Composable
+fun AnimatedIconRow(
+    icon: TodoIcon,
+    onIconChange: (TodoIcon) -> Unit,
+    modifier: Modifier = Modifier,
+    visible: Boolean = true
+) {
+    val enter = remember {
+        fadeIn(animationSpec = TweenSpec(300, easing = FastOutLinearInEasing))
+    }
+    val exit = remember {
+        fadeOut(animationSpec = TweenSpec(100, easing = FastOutSlowInEasing))
+    }
+    Box(modifier = modifier.defaultMinSize(minHeight = 16.dp)) {
+        AnimatedVisibility(
+            visible = visible,
+            enter = enter,
+            exit = exit
+        ) {
+            IconRow(icon = icon, onIconChange = onIconChange)
+        }
+    }
+}
+
+@Composable
+fun IconRow(
+    icon: TodoIcon,
+    onIconChange: (TodoIcon) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier) {
+        for (todoIcon in TodoIcon.values()) {
+            SelectableIconButton(
+                icon = todoIcon.imageVector,
+                iconContentDescription = todoIcon.contentDescription,
+                onIconSelected = { onIconChange(todoIcon) },
+                isSelected = (todoIcon == icon)
+            )
+        }
+    }
+}
+
+@Composable
+fun SelectableIconButton(
+    icon: ImageVector,
+    iconContentDescription: Int,
+    onIconSelected: () -> Unit,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier
+) {
+    //图标选中和未选中状态，颜色不一样
+    val tint = if (isSelected) {
+        MaterialTheme.colors.primary
+    } else {
+        MaterialTheme.colors.onSurface.copy(alpha = 0.6f)
+    }
+    TextButton(
+        onClick = { onIconSelected() },
+        shape = CircleShape,
+        modifier = modifier
+    ) {
+        Column {
+            Icon(
+                imageVector = icon,
+                tint = tint,
+                contentDescription = stringResource(id = iconContentDescription)
+            )
+            if (isSelected) {
+                Box(
+                    modifier = Modifier
+                        .padding(top = 3.dp)
+                        .width(icon.defaultWidth)
+                        .height(1.dp)
+                        .background(tint)
+                )
+            } else {
+                Spacer(modifier = Modifier.height(4.dp))
+            }
+        }
     }
 }
